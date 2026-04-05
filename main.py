16508e925dc4ad331bd97e791b4b4f3450621b89
Tahir Pathan <tahir.pathan88@gmail.com>
2026-04-05 20:33:39 +0530

    fix: render port binding and cors update

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from fastapi import FastAPI
 from src.tracker.router import router as tracker_router
 from src.user.router import router as user_router
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import os
 
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000", ## Frontend loacalhost
+    "http://localhost:3000",
+    os.getenv("FRONTEND_URL", ""),
 ]
 app.add_middleware(
     CORSMiddleware,
@@ -19,5 +22,7 @@ app.add_middleware(
 app.include_router(tracker_router)
 app.include_router(user_router)
 
-
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
 
